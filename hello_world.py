4fd1a8a0306fdbc82e0d1eb69be32b1ba89451d0
RisenDawn <hongtonatthawee@gmail.com>
2021-11-24 18:36:30 +0700

    Revert "Revert "Squashed commit of the following:""
    This reverts commit 25c2109e2fe1c1adf03bb5435f6489aa1ec8918b.

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
-print("HEE:LELELLELELELWOOW ORLD")
+print("Oh the misery")
+print("Everybody wants to be my enemyyyyyy")
+print("Spare the sympathy")
+print("Everybody wants to beeeeeeeeeee")
+print("my enemy")
